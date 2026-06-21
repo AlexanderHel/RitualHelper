7b80194b1306a02a97ec5643f44a935e8d8f1784
SmashPhoenix272 <smashphoenix@gmail.com>
2026-06-21 22:39:44 +0700

    fix: hide overlay when game is not the active foreground window

--- RitualHelperCore.cs
+++ RitualHelperCore.cs
@@ -66,7 +66,7 @@ namespace RitualHelper
                 return;
             }
 
-            if (Core.States.GameCurrentState != GameStateTypes.InGameState)
+            if (Core.States.GameCurrentState != GameStateTypes.InGameState || !Core.Process.Foreground)
             {
                 return;
             }
